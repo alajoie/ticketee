5f5e79d0d5acc6db619a65678e959acbf8dd8ef4
Alexis La Joie <alajoie@redpointtech.com>
2011-11-05 14:56:21 -0500

    Added feature for signing in via the Devise-provided form

--- features/signing_in.feature
+++ features/signing_in.feature
@@ -4,8 +4,16 @@ Feature: Signing in
 	I want to be able to sign in
 
 	Scenario: Signing in via confirmation
-		Given a registered user
-		And the registered user opens the confirmation email
-		And they click the first link in the email
-		Then a the account confirmation message should be shown
-		And the registered user should be signed in
+		Given I am a registered, unconfirmed user
+		And I open the confirmation email
+		And I click the first link in the email
+		Then the account confirmation message should be shown
+		And I should be signed in
+
+	Scenario: Signing in via form
+		Given I am a registered and confirmed user
+		And I am on the homepage
+		When I go to the sign in page
+		And sign in via the form
+		Then I should see a sign in success message
+		And I should be signed in 
--- features/step_definitions/sign_in_steps.rb
+++ features/step_definitions/sign_in_steps.rb
@@ -1,17 +1,42 @@
-Given /^a registered user$/ do
-  @user_address = "user@ticketee.com"
-  @user = User.create!(:email => @user_address, :password => "password")
+def create_user( address, password, confirmed)
+  @user = User.create!(:email => address, :password => password)
+  @user.confirm! if confirmed
 end
 
-Given /^the registered user opens the confirmation email$/ do
+
+Given /^I am a registered, unconfirmed user$/ do
+  @user_address = "user@ticketee.com"
+  create_user(@user_address, "password", false)
+end
+
+Given /^I am a registered and confirmed user$/ do
+  @user_address = "user@ticketee.com"
+  create_user(@user_address, "password", true)
+end
+
+Given /^I open the confirmation email$/ do
   open_email(@user_address, :with_subject => "Confirmation instructions")
 end
 
-Then /^a the account confirmation message should be shown$/ do
+Then /^the account confirmation message should be shown$/ do
   page.should have_content("Your account was successfully confirmed")
 end
 
-Then /^the registered user should be signed in$/ do
+Then /^I should be signed in$/ do
   page.should have_content("Signed in as " + @user_address)
+end
+
+When /^I go to the sign in page$/ do
+  click_link("Sign in")
+end
+
+When /^sign in via the form$/ do
+  fill_in("Email", :with => @user_address)
+  fill_in("Password", :with => "password")
+  click_button("Sign in")
+end
+
+Then /^I should see a sign in success message$/ do
+  page.should have_content("Signed in successfully.")
 end
 
